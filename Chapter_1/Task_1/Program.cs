629ea526d2b5f49a018d5dad3c64e4f227aa5955
kirillkirill1983 <baevkirill1983@gmail.com>
2021-04-19 10:41:37 +0300

    no message

--- Chapter_1/Task_1/Program.cs
+++ Chapter_1/Task_1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Task_1
 {
@@ -10,6 +7,42 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
+            Book<int> book = new Book<int>("Book1", 12);
+            book.Show();
+
+            Book<double> book1 = new Book<double>("Book2", 15.7);
+            book1.Show();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Book<T>
+    {
+        private string name;
+        private T price;
+
+        public Book(string nName, T pPrice)
+        {
+            this.name = nName;
+            this.price = pPrice;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public T Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"{Name} book {Price} price");
         }
     }
 }
