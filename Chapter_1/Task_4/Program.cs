4e5365c1b0cfd564291973414ec1abc6dcf40db0
kirillkirill1983 <baevkirill1983@gmail.com>
2021-04-20 19:58:55 +0300

    no message

---
+++ Chapter_1/Task_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClassTest myClassTest = new MyClassTest();
+            MyClass<MyClassTest> myClass = new MyClass<MyClassTest>();
+            MyClassTest obj = MyClass<MyClassTest>.FacthoryMethod();
+            Console.WriteLine($"{obj.GetType()}");
+            
+            obj.Name = "beer";
+            Console.WriteLine(obj.Name);
+
+            Console.ReadKey();
+        }
+    }
+    class MyClass<T> where T: new()
+    {
+        public static T FacthoryMethod() 
+        {
+            return new T() ;
+        }
+    }
+
+    class MyClassTest
+    {
+        public MyClassTest() { }
+        public string Name { get; set; }
+    }
+}
