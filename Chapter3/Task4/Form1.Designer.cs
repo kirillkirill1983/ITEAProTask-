cd307300fe465ced81270a9250af15d79f081c44
kirillkirill1983 <baevkirill1983@gmail.com>
2021-04-28 18:49:18 +0300

    no message

--- Chapter3/Task4/Form1.Designer.cs
+++ Chapter3/Task4/Form1.Designer.cs
@@ -55,6 +55,7 @@ namespace Task4
             this.button1.TabIndex = 1;
             this.button1.Text = "Старт";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -64,6 +65,7 @@ namespace Task4
             this.button2.TabIndex = 2;
             this.button2.Text = "Стоп";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -73,6 +75,7 @@ namespace Task4
             this.button3.TabIndex = 3;
             this.button3.Text = "Пауза";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
@@ -80,6 +83,10 @@ namespace Task4
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(224, 20);
             this.textBox1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -92,7 +99,7 @@ namespace Task4
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Задание Таймер";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Chapter3/Task4/Form1.cs
+++ Chapter3/Task4/Form1.cs
@@ -1,20 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace Task4
 {
     public partial class Form1 : Form
     {
+        DateTime date1 = new DateTime(0, 0);
         public Form1()
         {
             InitializeComponent();
+            
+            timer1.Tick += timer1_Tick;
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            date1 = date1.AddMilliseconds(1);
+            textBox1.Text = date1.ToString("mm:ss:ff");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            date1 = new DateTime(0, 0);
+            textBox1.Text = date1.ToString("mm:ss:ff");
+            timer1.Enabled = false;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
         }
     }
 }
