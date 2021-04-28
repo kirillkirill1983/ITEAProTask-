8c08607e92919e19e8a96c643f2cebf6d06af9ea
kirillkirill1983 <baevkirill1983@gmail.com>
2021-04-28 11:47:04 +0300

    Задание 1

---
+++ Chapter3/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        public delegate double MyDeligate(int a, int v, int c);
+
+        static void Main(string[] args)
+        {
+            int aInt = 1;
+            int bInt = 1;
+            int cInt = 2;
+            double result = 0;
+            MyDeligate myDeligate = delegate (int a, int b, int c) { return ((double)a + (double)b +(double) c) / 3; };
+            result = myDeligate(aInt,bInt,cInt);
+            Console.WriteLine($"{(double)result:F2}");
+
+            Console.ReadKey();
+        }
+    }
+}
