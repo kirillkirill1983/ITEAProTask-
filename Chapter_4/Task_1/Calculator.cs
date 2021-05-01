08abc448fbb31560a54ecd997dd99c9a46633fa2
kirillkirill1983 <baevkirill1983@gmail.com>
2021-05-01 09:53:14 +0300

    Задание 4

---
+++ Chapter_4/Task_1/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Calculator
+    {
+       
+        public Calculator()
+        { }
+        
+
+        public dynamic Add(int var1, int var2)
+        {
+            Console.WriteLine($"{var1} + {var2} = {var1 + var2}");
+            return  var1 + var2;
+        }
+        public dynamic Sub(int var1, int var2)
+        {
+            Console.WriteLine($"{var1} - {var2} = {var1 - var2}");
+            return var1 - var2;
+        }
+        public dynamic Mult(int var1, int var2)
+        {
+            Console.WriteLine($"{var1} * {var2} = {var1 * var2}");
+            return var1 * var2;
+        }
+        public dynamic Division(double var1, double var2)
+        {
+            dynamic result = 0;
+            if (var1 != 0 && var2 != 0)
+            {
+                result = var1 / var2;
+                return result;
+                Console.WriteLine("{0:F3}", result);
+                
+            }
+            else
+                Console.WriteLine("Деление на ноль ");
+                return 0;            
+
+        }
+    }
+
+}
+
---
+++ Chapter_4/Task_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator(); 
+            Console.WriteLine("Введите выполяемую арифметическую операцию");
+            var operatorEnter = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Введите первый оператор => ");
+            var operatorOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второй оператор => ");
+            var operatorTwo = Convert.ToInt32(Console.ReadLine());
+
+            switch (operatorEnter)
+            {
+                case "+":
+                    Console.WriteLine("Операция сложение ");
+                    calculator.Add(operatorOne, operatorTwo);
+                    break;
+                case "-":
+                    Console.WriteLine("Операция вычетания ");
+                    calculator.Sub(operatorOne, operatorTwo);
+                    break;
+                case "*":
+                    Console.WriteLine($"Операция умножения ");
+                    calculator.Mult(operatorOne, operatorTwo);
+                    break;
+                case "/":
+                    Console.WriteLine($"Операция деление");
+                    double opoperatorOneDoble = (double)operatorOne;
+                    double opoperatorTwoDoble = (double)operatorTwo;
+                    calculator.Division(opoperatorOneDoble, opoperatorTwoDoble);
+
+                    break;
+
+                default:
+                    Console.WriteLine("Такой операции нет ");
+                    break;
+            }
+            Console.ReadKey();
+        }
+
+        
+    }
+}
