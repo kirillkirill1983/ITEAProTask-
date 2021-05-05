93a3474db0e6b3297a5f90b61ba1165fb46245bf
kirillkirill1983 <baevkirill1983@gmail.com>
2021-05-05 09:31:14 +0300

    no message

--- Chapter_4/Task_2/Program.cs
+++ Chapter_4/Task_2/Program.cs
@@ -10,6 +10,49 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
+            List<Auto> autos = new List<Auto>
+            {
+                new Auto{MarkaAuto="гонка",Model="Запорожец",Year=1982,Color="Фиолетовый" },
+                new Auto{MarkaAuto="полугонка",Model="Таврия",Year=1981,Color="Оранжевый" },
+                new Auto{MarkaAuto="едет",Model="Москвич",Year=1985,Color="Зеленый" }
+            };
+
+            List<Driver> drivers = new List<Driver>
+            {
+                new Driver{ MarkaAuto="гонка", Name="Иван Иваныч", Tel="12-34-45"  },
+                new Driver{ MarkaAuto="полугонка", Name="Иван Фиоипыч", Tel="12-34-45"  },
+                new Driver{ MarkaAuto="гонка", Name="Семен Иваныч", Tel="12-34-45"  },
+                new Driver{ MarkaAuto="едет", Name="Николый Анросович", Tel="12-34-45"  },
+            };
+
+            var result = from select1 in autos
+                         join select2 in drivers
+                         on select1.MarkaAuto equals select2.MarkaAuto
+                         select new {Name=select2.Name,
+                             Marka=select2.MarkaAuto,
+                             Model=select1.Model,
+                             Year=select1.Year };
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Name} - {item.Marka} ({item.Model} {item.Year}) ");
+            }
+
+            Console.ReadKey();
         }
+    }
+    class Auto
+    {
+        public string MarkaAuto { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+
+    class Driver
+    {
+        public string MarkaAuto { get; set; }
+        public string Name { get; set; }
+        public string Tel { get; set; }
     }
 }
