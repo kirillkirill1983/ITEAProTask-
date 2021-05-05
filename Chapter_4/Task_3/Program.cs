e9b5849017a31ba1518f93bccca81f2ad7bcd5c9
kirillkirill1983 <baevkirill1983@gmail.com>
2021-05-05 09:47:00 +0300

    no message

--- Chapter_4/Task_3/Program.cs
+++ Chapter_4/Task_3/Program.cs
@@ -10,6 +10,17 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
+            int[] arrayRangr = { -2,1, 2, 3, 4, 5, 2, 34, -2, 5656, 212, 4, 5, -67, 34, -6 ,4,5};
+
+            var positiveNums = arrayRangr.Where(i => i >= 0);
+            var negativeNums = arrayRangr.Where(i => i < 0);
+            var first = positiveNums.First();
+            var last = negativeNums.Last();
+
+            Console.WriteLine($"первый положительный {first} " +
+                $"последний отрицательный {last}");
+
+            Console.ReadKey();
         }
     }
 }
