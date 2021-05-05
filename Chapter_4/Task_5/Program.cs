ae6803ffd84301875e2df4ddf0d9f3ad97824617
kirillkirill1983 <baevkirill1983@gmail.com>
2021-05-05 11:44:06 +0300

    no message

--- Chapter_4/Task_5/Program.cs
+++ Chapter_4/Task_5/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace Task_5
 {
+    delegate dynamic ArraDeligate(dynamic dynamic);
     class Program
     {
         static void Main(string[] args)
         {
+            dynamic[] numbers = { "Auto-Aвто","Driver-Водитель","Beer-Пиво","Color-Цвет"};
+
+            ArraDeligate MoreThanTen = delegate (dynamic i) { return i ; };
+
+            var result = (MoreThanTen(numbers));
+
+            foreach (dynamic i in result)
+                Console.WriteLine(i);
+
+            Console.ReadKey();
         }
     }
 }
