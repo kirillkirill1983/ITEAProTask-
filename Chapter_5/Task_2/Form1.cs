15336ec7d4f22aace07ee013c38a6829fbca6836
kirillkirill1983 <baevkirill1983@gmail.com>
2021-05-07 14:03:52 +0300

    no message

--- Chapter_5/Task_2/Form1.cs
+++ Chapter_5/Task_2/Form1.cs
@@ -12,9 +12,19 @@ namespace Task_2
 {
     public partial class Form1 : Form
     {
+        private Model1Container model1Container;
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            model1Container = new Model1Container();
+           
+            dataGridView1.DataSource = model1Container.Entity1Set.Local;
+           
         }
     }
 }
