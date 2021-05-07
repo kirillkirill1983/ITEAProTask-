dbe67ac9908722ff16c7bca54587ed4c80502665
kirillkirill1983 <baevkirill1983@gmail.com>
2021-05-07 14:47:49 +0300

    no message

--- Chapter_5/Task_2/Form1.cs
+++ Chapter_5/Task_2/Form1.cs
@@ -22,6 +22,7 @@ namespace Task_2
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             model1Container.Entity1Set.Add(new Entity1
             {
                 Id = 1,
@@ -35,7 +36,7 @@ namespace Task_2
                 City = "Gopa"
             });
             dataGridView1.DataSource = model1Container.Entity1Set.Local;
-           
+            dataGridView1.Refresh();
         }
     }
 }
