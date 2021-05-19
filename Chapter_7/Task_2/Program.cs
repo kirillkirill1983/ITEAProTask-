cf34e38ac2149207ae6d71405bb968fd7a38ad71
kirillkirill1983 <baevkirill1983@gmail.com>
2021-05-19 14:59:46 +0300

    no message

---
+++ Chapter_7/Task_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream(@"G:\Temp_temp\text.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            using (StreamWriter writer = new StreamWriter(fileStream, Encoding.Unicode)
+)
+            {
+                writer.WriteLine("Привет");
+
+                writer.WriteLine("Мир )) ");
+
+            }
+
+            Console.WriteLine("Файл Text.txt создан и в него записан текст.");
+
+            
+            Console.WriteLine("Содержимое файла Text.txt:\n");
+
+            using (StreamReader reader = File.OpenText(@"G:\Temp_temp\text.txt"))
+            {
+                string input;
+
+                while ((input = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(input);
+                }
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter_7/Task_3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string catalog = @"G:\Temp_temp";
+
+            string fileName = "text1.txt";
+
+            //проводим поиск в выбранном каталоге и во всех его подкаталогах
+            string path = null;
+            foreach (string findedFile in Directory.EnumerateFiles(catalog, fileName,
+                SearchOption.AllDirectories))
+            {
+                FileInfo FI= new FileInfo(findedFile);
+                Console.WriteLine(FI.Name + " " + FI.FullName + " " + FI.Length + "_байт" +
+                        " Создан: " + FI.CreationTime);
+                path = FI.FullName;
+            }
+            
+            if (path != null)
+            {
+                
+                using (StreamReader reader = File.OpenText(path))
+                {
+                    string input;
+
+                    while ((input = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine(input);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файла нет");
+            }
+            Console.ReadKey();
+        }
+    }
+}
