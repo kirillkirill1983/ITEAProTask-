731e6dc5e0e000c34419fb141038ed64280fabf8
kirillkirill1983 <baevkirill1983@gmail.com>
2021-05-27 14:20:51 +0300

    no message

--- Chapter_10/Task/Program.cs
+++ Chapter_10/Task/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Task
 {
@@ -10,8 +9,29 @@ namespace Task
     {
         static void Main(string[] args)
         {
+            Person person = new Person("Kirill",38);
+            person.AgeNew();
+
+            FileStream stream = File.Create("MyData.dat");
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, person);
+            stream.Close();
+
+            // Открывает существующий файл для чтения.
+            stream = File.OpenRead("MyData.dat");
+
+            // Десериализация.
+            Person personDeserialize = formatter.Deserialize(stream) as Person;
+
+            Console.WriteLine(personDeserialize.Age);
+            Console.WriteLine(personDeserialize.Name);
+            personDeserialize.AgeNew();
+            
+
+            Console.ReadKey(); 
         }
     }
+    [Serializable]
     public class Person
     {
         public string Name { get; set; }
@@ -26,5 +46,10 @@ namespace Task
             Name = name;
             Age = age;
         }
+        public void AgeNew() 
+        {
+            Console.WriteLine($"{Convert.ToByte(Age)}");
+        }
+       
     }
 }
